2c1e5f1dab8876eee3bdabe7da19a86924a6488c
Niklas-Hakmann <nikha22@student.sdu.dk>
2024-10-30 12:25:50 +0100

    Update World.cs
    ændret tilbage

--- World.cs
+++ World.cs
@@ -5,17 +5,18 @@ class World {
   Space entry;
   
   public World () {
-    Space beehive    = new Space("Beehive");
-    Space have1 = new Space("Have1");
-    Space have2_0  = new Space("Have2_0");
-    Space have2_1  = new Space("Have2_1");
-    Space have2_2  = new Space("Have2_2");
+    Space entry    = new Space("Beehive");
+    Space corridor = new Space("Corridor");
+    Space cave     = new Space("Cave");
+    Space pit      = new Space("Darkest Pit");
+    Space outside  = new Space("Outside");
     
-    beehive.AddEdge("beehive", beehive);
-    have1_0.AddEdge("have1", have1);
-    have2_0.AddEdge("have2_0", have2_0);
-    have2_1.AddEdge("have2_1", have2_1);
-    have2_2.AddEdge("have2_2", have2_2);
+    entry.AddEdge("door", corridor);
+    corridor.AddEdge("door", cave);
+    cave.AddEdge("north", pit);
+    cave.AddEdge("south", outside);
+    pit.AddEdge("door", cave);
+    outside.AddEdge("door", cave);
     
     
     this.entry = entry;
