595747b9bf33ba32266824e2d4843e90f93a2bb4
Tobias Ralsted <tralsted@hotmail.dk>
2024-11-01 11:16:14 +0100

    Tilføjet try-catch for go command.

--- CommandGo.cs
+++ CommandGo.cs
@@ -7,10 +7,16 @@ class CommandGo : BaseCommand, ICommand {
   }
   
   public void Execute (Context context, string command, string[] parameters) {
+    try {
     if (GuardEq(parameters, 1)) {
       Console.WriteLine("I don't seem to know where that is 🤔");
       return;
     }
     context.Transition(parameters[0]);
+    }
+    catch {
+      Console.WriteLine("Du fløj ind i hækken. prøv en mulig vej.");
+
+    }
   }
 }
