0fe5106f6dba1eac5b9acfd0308fafd2667f34b2
Niklas-Hakmann <nikha22@student.sdu.dk>
2024-11-04 11:55:11 +0100

    Create Item.cs

---
+++ Item.cs
@@ -0,0 +1,26 @@
+public class Item
+{
+    //Initializing name of item, and if item is being held.
+    public string name;
+    public bool holding_item;
+
+    public Item(string name_val, bool holding_item_val)
+    {
+        name = name_val;
+        holding_item = holding_item_val;
+    }
+    // Method for getting name of item
+    // To retrieve name of item: itemName.GetName();
+    public string GetName()
+    {
+        return name;  
+    }
+    // Properties of HoldingItem
+    // To create new item: Item itemName = new Item("name", false);
+    // To change item to being held : itemName.HoldingItem = true;
+    public bool HoldingItem
+    {
+        get { return holding_item; }
+        set { holding_item = value; }
+    }
+}
